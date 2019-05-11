bdfd7ba9caa5225507018b30e7d9815d34283262
Udit Mittal <udit7590@users.noreply.github.com>
2019-05-11 23:14:49 +0530

    Refactors update_quality method and adds logic for Conjured goods

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,53 +3,60 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_daily_quality(item)
+      update_daily_sellin(item)
     end
   end
+
+  private
+
+    def update_daily_quality(item)
+      case item.name
+      when 'Aged Brie'
+        increase_quality_for_product(item)
+        increase_quality_for_product(item) if (item.sell_in == 0)
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        if ((item.sell_in - 1) < 0)
+          item.quality = 0
+        else
+          increase_quality_for_product(item)
+          increase_quality_for_product(item) if item.sell_in < 11
+          increase_quality_for_product(item) if item.sell_in < 6
+        end
+      when 'Sulfuras, Hand of Ragnaros'
+        # Quality does not change for Sulfuras
+      when 'Conjured'
+        decrease_quality_for_product(item, 2)
+        decrease_quality_for_product(item, 2) if (item.sell_in == 0)
+      else
+        decrease_quality_for_product(item)
+        decrease_quality_for_product(item) if (item.sell_in == 0)
+      end
+    end
+
+    def update_daily_sellin(item)
+      case item.name
+      when 'Sulfuras, Hand of Ragnaros'
+        # Sell In does not matter for Sulfuras
+      else
+        item.sell_in = item.sell_in - 1
+      end
+    end
+
+    def increase_quality_for_product(item, count=1)
+      if item.quality < 50
+        item.quality = item.quality + count
+      end
+    end
+
+    def decrease_quality_for_product(item, count=1)
+      if item.quality > 0
+        item.quality = item.quality - count
+      end
+    end
+
 end
 
 class Item
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -5,7 +5,90 @@ describe GildedRose do
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+      expect(items[0].name).to eq 'foo'
     end
+
+    it 'decreases sell_in value by 1 for all types of goods' do
+      items = [Item.new('foo', 1, 1), Item.new('Aged Brie', 1, 1), Item.new('Backstage passes to a TAFKAL80ETC concert', 1, 1), Item.new('Conjured', 1, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items.map(&:sell_in)).to eq [0, 0, 0, 0]
+    end
+
+    it 'never decreases quality to a negative number' do
+      items = [Item.new('foo', 2, 0), Item.new('foo', 0, 0), Item.new('Aged Brie', 2, 0), Item.new('Aged Brie', 0, 0), Item.new('Backstage passes to a TAFKAL80ETC concert', 2, 0), Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 0), Item.new('Conjured', 2, 0), Item.new('Conjured', 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items.map(&:quality)).to eq [0, 0, 1, 2, 3, 0, 0, 0]
+    end
+
+    it 'never changes quality for Sulfuras' do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 80), Item.new('Sulfuras, Hand of Ragnaros', 2, 80), Item.new('Sulfuras, Hand of Ragnaros', 14, 80)]
+      GildedRose.new(items).update_quality()
+      expect(items.map(&:quality)).to eq [80, 80, 80]
+    end
+
+    context 'sell_in has not expired' do
+      it 'decreases quality by 1 for goods other than Sulfuras, Aged Brie and Backstage passes' do
+        items = [Item.new('foo', 2, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 1
+      end
+
+      it 'decreases quality by 2 for Conjured goods' do
+        items = [Item.new('Conjured', 2, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+
+      it 'increases quality by 1 for Aged Brie' do
+        items = [Item.new('Aged Brie', 2, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 3
+      end
+
+      it 'increases quality by 1 for Backstage passes if sell_in is more than 10' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 3
+      end
+
+      it 'increases quality by 2 for Backstage passes if sell_in is less than 10 but greater than 5' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 4
+      end
+
+      it 'increases quality by 3 for Backstage passes if sell_in is less than 5' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 5
+      end
+    end
+
+    context 'sell_in has expired' do
+      it 'decreases quality by 2 for goods other than Sulfuras, Aged Brie and Backstage passes' do
+        items = [Item.new('foo', 0, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+
+      it 'decreases quality by 4 for Conjured goods' do
+        items = [Item.new('Conjured', 0, 4)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+
+      it 'increases quality by 2 for Aged Brie' do
+        items = [Item.new('Aged Brie', 0, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 4
+      end
+
+      it 'decreases quality to 0 for Backstage passes' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(0)
+      end
+    end
+
   end
 end
